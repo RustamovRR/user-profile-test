d050e8756bedafa4525887ecbc5cfea25b35a90d
@riskiddin.rustamov <riskiddin.rustamov@paynet.uz>
2024-07-30 20:44:22 +0500

    feat: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import { useProfileQuery } from "./hooks";
 function App() {
   const { data } = useProfileQuery();
 
-  return <div>{data?.address.city}</div>;
+  return (
+    <div>
+      {data?.address.city}
+      <h1 className="text-red-200">test</h1>
+    </div>
+  );
 }
 
 export default App;
